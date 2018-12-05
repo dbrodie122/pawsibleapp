4e92d1fd68cbacd238e4ed7f3bf72a4442686f7a
David Brodie <david.brodie122@gmail.com>
2018-12-04 21:43:43 -0500

    initial commit

---
+++ pawsible-db/Pet.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Pet 
+(
+  PetId int not null primary key identity(1,1)
+, PetName varchar (100) not null
+, Breed varchar (100) not null default 'Unknown'
+, OwnerName varchar (100) not null
+, Notes varchar (3000) null
+)
