adb74c0bc0e0f54d8eb4dcfb75da9a23c21a3302
David Brodie <david.brodie122@gmail.com>
2019-03-07 21:05:02 -0500

    Trying to queue a CI build and release.

--- pawsible-db/dbo/Pet.sql
+++ pawsible-db/dbo/Pet.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Pet 
 (
   PetId int not null primary key identity(1,1)
-, PetName varchar (100) not null
+, PetName varchar (100) not null 
 , Breed varchar (100) not null default 'Unknown'
 , OwnerName varchar (100) not null
 , Notes varchar (3000) null
